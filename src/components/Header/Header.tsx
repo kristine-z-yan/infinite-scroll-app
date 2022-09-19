1341b42dd669125784340ec5c6fd49d9ce6a8204
Kristin <qristinehzaqarian@gmail.com>
2022-09-19 16:09:59 +0400

    Initial

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import styles from './Header.module.scss';
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                Instagram
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header;
